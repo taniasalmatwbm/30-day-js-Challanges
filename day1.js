ad5e5a228c5da0c9f25fcc51b76e703eac8f1ab7
taniasalmatwbm <taniasalmatwbm@gmail.com>
2024-07-13 14:32:46 +0500

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,84 @@
+// Activity -------1 
+//Task-1 
+//Declare a variable using var , assign it is a number, and log the value to the console-
+ 
+var num2 = 5;
+console.log(num2)
+
+//Task2
+//Declare a variable using let , assign it is a string, and log the value to the console-
+
+let userName= "taniya"
+console.log(userName)
+
+//Activity-----2
+//Constant Declaration
+//Task 3 
+//Declare a variable using const , assign it is boolean value, and log value on console-
+
+const active= true
+console.log(active)
+
+//Activity 3
+//Task 4
+//create a variable of different datatype (number, string, boolean, obj, array) and 
+//log each variable type using the type of variable-
+ var num1= 20;
+ console.log(typeof num1)
+
+ let namePerson= "Maria";
+ console.log(typeof namePerson)
+
+ const available= true;
+ console.log(typeof available)
+
+   let obj= {
+    name: "taniya",
+    class: 16,
+    active: true
+   }
+console.log(typeof obj)
+
+   let array =[1, 2,,3,4,5]
+   console.log(typeof array)
+
+
+//Activity 4
+//Declare a variable using let assign it an initial value, reasign a new value and log 
+//both value on console-
+
+let initialValue1= 10;
+console.log(initialValue1)
+initialValue1 = 20;
+console.log(initialValue1)
+//console.log(initialValue1)
+
+
+ const num =10;
+ console.log(num)
+
+ //num =30
+
+ //SyntaxError: Identifier 'num' has already been declared
+//Feature Request
+ // write a script that declare variables of different data type and log the both value 
+ //and type of each variable to the console-
+
+ //Ans
+ //sting variable typeof string
+ //boolean variable typeof boolean
+ //num variable typeof num 
+ // object and array considered both typeof as object in javascript 
+
+
+ //Reassignment Demo :
+ //Create a script that demonstrates difference in behavior let and const 
+ //when it come to ressignment-
+ //Ans
+ // when we use let and assign a value and then we reassign a new value with same name
+ // in console value will be change so let is muteable
+
+ // with const we cannot reassign a new value to constant variable const 
+ // is immutable
+
+ //please cheackout my all question
